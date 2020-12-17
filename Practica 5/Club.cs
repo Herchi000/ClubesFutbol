74f695987b1605644f44f8fc217b88837ed09d80
Emanuel Fernandez <hugofer000@gmail.com>
2020-12-17 14:53:50 -0300

    Primer punto de guardado

---
+++ Practica 5/Club.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Practica_5.Program;
+
+namespace Practica_5
+{
+	interface IColorVentana
+	{
+		public void CambiarVentana();
+
+	}
+
+
+
+
+
+
+	//------------------------------------------------
+	class Club
+	{
+		private string nombre;
+		private string nombreClub;
+		private string nombreEstadio;
+
+
+		private string[] opcionesClub = new string[]
+		{
+			"1. Comprar Entrada",
+			"2. Hacerte Socio",
+			"3. Ver calendario",
+			"4. Salir"
+		};
+
+		public Club(string nombreClub, string nombreEstadio)
+		{
+			this.nombreClub = nombreClub;
+			this.nombreEstadio = nombreEstadio;
+		}		
+
+		public void ElegirOpcion()
+		{
+			Console.WriteLine("			Bienvenido al sitio de {0}", nombreClub);
+
+			byte opc = (byte)ElegirOpc.SeleccionOpc("\nIngrese la operación que desea realizar", opcionesClub, 1, 4);
+
+			switch (opc)
+			{
+				case 1: comprarEntrada(); break;
+				case 2: Console.WriteLine("Eligió la opción 2"); break;
+				case 3: Console.WriteLine("Eligió la opción 3"); break;
+			}
+
+			//Console.WriteLine("\nIngrese la operación que desea realizar");
+
+		}
+
+
+		public virtual void CambiarVentana()
+		{
+			
+		}
+
+		public void SetNombre()
+		{
+			Console.Write("Ingrese su nombre: ");
+			this.nombre = Console.ReadLine();
+		}
+
+		protected void comprarEntrada()
+		{
+			int cantidad = 0;
+			bool AuxBool = false;
+			string[] opcionesEntradas =
+			{
+				"1. Vista Lejana - $3000",
+				"2. Vista Media - $5000",
+				"3. Vista Cercana - $10000"
+
+			};
+
+			int opc = ElegirOpc.SeleccionOpc("				Comprar Entrada \nElija su lugar en " + nombreEstadio+"\n", opcionesEntradas, 1, 3);
+
+			while (AuxBool == false)
+			{
+				Console.Write("Ingrese la cantidad de entradas a comprar. (Máximo 3): ");
+				if (!(int.TryParse(Console.ReadLine(), out cantidad))) Console.WriteLine("Error! Ingrese solo números");
+				else if (cantidad < 1 || cantidad > 3) Console.WriteLine("Error! Ingrese valores dentro del rango 1 - 3");
+				else AuxBool = true;
+			}
+
+			switch (opc)
+			{
+				case 1:
+					if (ElegirOpc.ConfirmarOpcion(($"1. Vista Lejana - $3000 x {cantidad} \n¿Confirmar?: ")))
+					{
+						Console.WriteLine("Gracias por la compra.");
+					}
+					else Console.WriteLine("Usted canceló la operación");
+					break;
+				case 2:
+					if (ElegirOpc.ConfirmarOpcion(($"2. Vista Media - $5000 x {cantidad} \n¿Confirmar?: ")))
+					{
+						Console.WriteLine("Gracias por la compra.");
+					}
+					else Console.WriteLine("Usted canceló la operación");
+					break;
+				case 3:
+					if (ElegirOpc.ConfirmarOpcion(($"3. Vista Cercana - $10000 x {cantidad} \n¿Confirmar?: ")))
+					{
+						Console.WriteLine("Gracias por la compra.");
+					}
+					else Console.WriteLine("Usted canceló la operación");
+					break;
+			}
+
+			Console.ReadKey(true);
+			Console.Clear();
+			
+		}
+
+
+
+		public string GetNombre() => nombre;
+		public string GetNombreClub() => nombreClub;
+
+		public virtual void GetCalendario()
+		{
+
+		}
+	}
+
+
+
+	//-------------------------------------------------------------------------------------------------------------------------------
+	//CLASES HIJAS 
+
+
+	class River : Club
+	{
+		private string[] calendario = new string[]
+		{
+			"River vs Banfield. Fecha 1 Campeonado. 05/12/2020",
+			"Lanus vs River. Fecha 2 Campeonado. 12/12/2020",
+			"River vs Nacional. Partido Ida Copa Libertadores. 15/12/2020",
+			"Rosario Central vs River. Fecha 3 Campeonado. 19/12/2020",
+			"Nacional vs River. Partido Vuelta Copa Libertadores. 22/12/2020",
+			"River vs Godoy Cruz. Fecha 4 Campeonado. 26/12/2020"
+		};
+
+		public River(string nombreClub, string nombreEstadio): base(nombreClub, nombreEstadio)
+		{
+
+		}
+		public override void CambiarVentana()
+		{
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Title = "River Plate";
+		}
+
+
+		public override void GetCalendario()
+		{
+			foreach (string aux in calendario)
+			{
+				Console.WriteLine(aux);
+			}
+
+		}
+
+	}
+
+	class Boca: Club
+	{
+		private string[] calendario = new string[]
+		{
+			"Boca vs Newell's. Fecha 1 Campeonado. 05/12/2020",
+			"Velez vs Boca. Fecha 2 Campeonado. 12/12/2020",
+			"Boca vs Racing. Partido Ida Copa Libertadores. 15/12/2020",
+			"Defensa y Justicia vs Boca. Fecha 3 Campeonado. 19/12/2020",
+			"Racing vs Boca. Partido Vuelta Copa Libertadores. 22/12/2020",
+			"Boca vs Gimnasia. Fecha 4 Campeonado. 26/12/2020"
+		};
+
+		public Boca(string nombreClub, string nombreEstadio) : base(nombreClub, nombreEstadio)
+		{
+
+		}
+
+		public override void CambiarVentana()
+		{
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Title = "Boca Juniors";
+		}
+
+		public override void GetCalendario()
+		{
+			foreach (string aux in calendario)
+			{
+				Console.WriteLine(aux);
+			}
+
+		}
+	}
+
+	class Independiente: Club
+	{
+		private string[] calendario = new string[]
+		{
+			"Independiente vs Aldosivi. Fecha 1 Campeonado. 05/12/2020",
+			"Estudiantes LP vs Independiente. Fecha 2 Campeonado. 12/12/2020",
+			"Independiente vs Lanus. Partido Ida Copa Sudamericana. 15/12/2020",
+			"Colon vs Independiente. Fecha 3 Campeonado. 19/12/2020",
+			"Lanus vs Independiente. Partido Vuelta Copa Sudamericana. 22/12/2020",
+			"Independiente vs Union. Fecha 4 Campeonado. 26/12/2020"
+		};
+		public Independiente(string nombreClub, string nombreEstadio) : base(nombreClub, nombreEstadio)
+		{
+
+		}
+
+		public override void CambiarVentana()
+		{
+			Console.BackgroundColor = ConsoleColor.DarkRed;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Title = "Independiente";
+		}
+
+		public override void GetCalendario()
+		{
+			foreach (string aux in calendario)
+			{
+				Console.WriteLine(aux);
+			}
+
+		}
+	}
+
+	class Racing: Club
+	{
+		private string[] calendario = new string[]
+		{
+			"Racing vs Patronato. Fecha 1 Campeonado. 05/12/2020",
+			"Huracan vs Racing. Fecha 2 Campeonado. 12/12/2020",
+			"Boca vs Racing. Partido Ida Copa Libertadores. 15/12/2020",
+			"Talleres vs Racing. Fecha 3 Campeonado. 19/12/2020",
+			"Racing vs Boca. Partido Vuelta Copa Libertadores. 22/12/2020",
+			"Racing vs Atl Tucuman. Fecha 4 Campeonado. 26/12/2020"
+		};
+
+		public Racing(string nombreClub, string nombreEstadio) : base(nombreClub, nombreEstadio)
+		{
+
+		}
+
+		public override void CambiarVentana()
+		{
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Title = "Racing Club";
+		}
+
+		public override void GetCalendario()
+		{
+			foreach (string aux in calendario)
+			{
+				Console.WriteLine(aux);
+			}
+
+		}
+	}
+
+	class SanLorenzo: Club
+	{
+		private string[] calendario = new string[]
+		{
+			"San Lorenzo vs Central Cordoba. Fecha 1 Campeonado. 05/12/2020",
+			"Arsenal vs San Lorenzo. Fecha 2 Campeonado. 12/12/2020",
+			"Argentinos Jrs vs San Lorenzo. Fecha 3 Campeonado. 19/12/2020",
+			"San Lorenzo vs Temperley. Fecha 4 Campeonado. 26/12/2020"
+		};
+		public SanLorenzo(string nombreClub, string nombreEstadio) : base(nombreClub, nombreEstadio)
+		{
+
+		}
+
+		public override void CambiarVentana()
+		{
+			Console.BackgroundColor = ConsoleColor.DarkRed;
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.DarkBlue;
+			Console.Title = "San Lorenzo";
+		}
+
+		public override void GetCalendario()
+		{
+			foreach (string aux in calendario)
+			{
+				Console.WriteLine(aux);
+			}
+
+		}
+	}
+}
---
+++ Practica 5/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practica_5
+{
+	class Program
+	{
+		static void Main()
+		{
+			string[] elegirClub = new string[]
+			{
+				"1. River ",
+				"2. Boca",
+				"3. Independiente",
+				"4. Racing ",
+				"5. San Lorenzo "
+			};
+
+
+			//ElegirClub.SeleccionClub();
+			int auxClub = ElegirOpc.SeleccionOpc("Elija el club del que es hincha", elegirClub, 0, 5);
+
+			List<Club> AuxUsuario = new List<Club> 
+			{
+				new River("River Plate", "El Monumental"),
+				new Boca("Boca Juniors", "La Bombonera"), 
+				new Independiente("Independiente", "El Libertadores de América"), 
+				new Racing("Racing Club", "El Cilindro"), 
+				new SanLorenzo("San Lorenzo", "El Nuevo Gasómetro") 
+			};
+			
+			Club usuario = AuxUsuario[auxClub-1];
+			usuario.SetNombre();
+
+			usuario.CambiarVentana();
+
+			usuario.ElegirOpcion();
+			
+			
+
+
+			Console.WriteLine(usuario.GetNombre());
+			Console.WriteLine(usuario.GetNombreClub());
+			
+		}
+
+
+		//Clase con múltiples métodos de gestión
+		public class ElegirOpc
+		{
+
+			/*******************
+			 * brief: Retorna un valor segun la opcion que se haya elegido de varias opciones
+			 * param: Mensaje a desplegar en consola
+			 * param: array de string que representan las multiples opciones
+			 * param: cantidad mínima de opciones a elegir
+			 * param: cantidad máxmima de opciones a elegir
+			 * return: Valor entero de la opcion elegida
+			 * ********************************************/
+			public static int SeleccionOpc(string mensaje, string[] opciones, int valMin, int valMax)
+			{
+				int auxInt;
+
+				Console.WriteLine(mensaje);
+
+				foreach (string str in opciones)
+				{
+					Console.WriteLine(str);
+				}
+
+
+
+				bool flag = false;
+				while (flag == false)
+				{
+					Console.Write("Opción: ");
+					if (!(int.TryParse(Console.ReadLine(), out auxInt)))
+					{
+						Console.WriteLine("Error! Ingrese solo números.");
+					}
+					else
+					{
+						if (auxInt < valMin || auxInt > valMax)
+						{
+							Console.WriteLine("Error! Ingrese un valor entre {0} y {1}.", valMin, valMax);
+						}
+						else
+						{
+							switch (auxInt)
+							{
+								case 1: flag = true; return 1;
+								case 2: flag = true; return 2;
+								case 3: flag = true; return 3;
+								case 4: flag = true; return 4;
+								case 5: flag = true; return 5;
+
+							}
+						}
+					}
+
+				}
+
+				return -1;
+
+			}
+
+			public static bool ConfirmarOpcion(string mensaje)
+			{
+				string confirmar ="";
+				bool AuxBool = false;
+				Console.WriteLine(mensaje);
+
+				while (AuxBool == false)
+				{
+					Console.Write("S/N: ");
+					confirmar = Console.ReadLine();
+
+					if ((confirmar[0] != 'S') && (confirmar[0] != 's') && (confirmar[0] != 'N') && (confirmar[0] != 'n'))
+					{
+						Console.WriteLine("Error! Ingrese solo \"S\" o \"N\"");
+
+					}
+					else
+					{
+						AuxBool = true;
+
+						
+					}
+				}
+
+
+				if ((confirmar[0] == 'S') || (confirmar[0] == 's')) return true;
+
+				else  return false;
+
+
+			}
+
+			/*
+			public static int setClub()
+			{
+				int auxInt;
+				bool flag = false;
+				while (flag == false)
+				{
+					Console.Write("Opción: ");
+					if (!(int.TryParse(Console.ReadLine(), out auxInt)))
+					{
+						Console.WriteLine("Error! Ingrese solo números.");
+					}
+					else
+					{
+						if (auxInt < 1 || auxInt > 5)
+						{
+							Console.WriteLine("Error! Ingrese un valor entre 1 y 5.");
+						}
+						else
+						{
+							switch (auxInt)
+							{
+								case 1: flag = true; return 0;	
+								case 2: flag = true; return 1;
+								case 3: flag = true; return 2;
+								case 4: flag = true; return 3;
+								case 5: flag = true; return 4;
+
+							}
+						}
+					}
+					
+				}
+
+				return -1;
+			}
+			*/
+		}
+	}
+}
